901c70e96c1af4c3849e62be2eaad04f2992bc19
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-05-09 23:48:00 +0700

    add class screen create

---
+++ lib/screen/create.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateSpotForm extends StatefulWidget {
+  const CreateSpotForm({super.key});
+
+  @override
+  State<CreateSpotForm> createState() => _CreateSpotFormState();
+}
+
+class _CreateSpotFormState extends State<CreateSpotForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
