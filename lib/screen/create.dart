d670f1132cc4c566d9123a53acb1369e1081edc3
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-05-09 23:49:15 +0700

    add var create

--- lib/screen/create.dart
+++ lib/screen/create.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 
 class CreateSpotForm extends StatefulWidget {
-  const CreateSpotForm({super.key});
-
   @override
   State<CreateSpotForm> createState() => _CreateSpotFormState();
 }
 
 class _CreateSpotFormState extends State<CreateSpotForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _namaTempatController = TextEditingController();
+  final _makananFavoritController = TextEditingController();
+  final _minumanFavoritController = TextEditingController();
+  final _noTeleponController = TextEditingController();
+  final _lokasiController = TextEditingController();
+  final _jamBukaController = TextEditingController();
+  final _jamTutupController = TextEditingController();
+
+  List<String> _jenisKuliner = [];
+  int _rating = 1;
+  File? _image;
+
+  Future<void> _getImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedImage != null) {
+        _image = File(pickedImage.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
