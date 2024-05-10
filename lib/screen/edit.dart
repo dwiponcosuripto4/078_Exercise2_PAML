cef4b7ec20dca16563d55ab3ab600205961313c0
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-05-10 09:05:51 +0700

    create class EditSpotForm

---
+++ lib/screen/edit.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditSpotForm extends StatefulWidget {
+  final Spot spot;
+
+  const EditSpotForm({Key? key, required this.spot}) : super(key: key);
+
+  @override
+  State<EditSpotForm> createState() => _EditSpotFormState();
+}
+
+class _EditSpotFormState extends State<EditSpotForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
