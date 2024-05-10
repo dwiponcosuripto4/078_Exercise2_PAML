8165a12c4c74d6565973cdc373dc8aa3f0c070cd
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-05-10 09:24:59 +0700

    add text to call data

--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -8,6 +8,65 @@ class DetailSpot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Spot'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              spot.namaTempat,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Jenis Kuliner: ${spot.jenisKuliner}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Makanan Favorit: ${spot.makananFavorit}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Minuman Favorit: ${spot.minumanFavorit}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Nomor Telepon: ${spot.noTelepon}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Lokasi: ${spot.lokasi}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Jam Buka: ${spot.jamBuka}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Jam Tutup: ${spot.jamTutup}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Rating: ${spot.rating}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
