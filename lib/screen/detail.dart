da45529b3c754a0269f513a2d340cd3146d36429
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-05-10 09:22:50 +0700

    create class Detail

---
+++ lib/screen/detail.dart
@@ -0,0 +1,13 @@
+import 'package:exercise2/model/Spot.dart';
+import 'package:flutter/material.dart';
+
+class DetailSpot extends StatelessWidget {
+  final Spot spot;
+
+  const DetailSpot({Key? key, required this.spot}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
