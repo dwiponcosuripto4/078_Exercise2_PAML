54cf77bda408c9e95b259a60fa586f49e8b24282
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-05-10 09:09:20 +0700

    fill var value

--- lib/screen/edit.dart
+++ lib/screen/edit.dart
@@ -1,3 +1,4 @@
+import 'package:exercise2/model/Spot.dart';
 import 'package:flutter/material.dart';
 
 class EditSpotForm extends StatefulWidget {
@@ -10,6 +11,32 @@ class EditSpotForm extends StatefulWidget {
 }
 
 class _EditSpotFormState extends State<EditSpotForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _namaTempatController = TextEditingController();
+  String _jenisKuliner = '';
+  final _makananFavoritController = TextEditingController();
+  final _minumanFavoritController = TextEditingController();
+  final _noTeleponController = TextEditingController();
+  final _lokasiController = TextEditingController();
+  final _jamBukaController = TextEditingController();
+  final _jamTutupController = TextEditingController();
+  final _ratingController = TextEditingController();
+  final SpotController _spotController = SpotController();
+
+  @override
+  void initState() {
+    super.initState();
+    _namaTempatController.text = widget.spot.namaTempat;
+    _jenisKuliner = widget.spot.jenisKuliner;
+    _makananFavoritController.text = widget.spot.makananFavorit;
+    _minumanFavoritController.text = widget.spot.minumanFavorit;
+    _noTeleponController.text = widget.spot.noTelepon;
+    _lokasiController.text = widget.spot.lokasi;
+    _jamBukaController.text = widget.spot.jamBuka;
+    _jamTutupController.text = widget.spot.jamTutup;
+    _ratingController.text = widget.spot.rating;
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
